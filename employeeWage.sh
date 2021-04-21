e48513b7e30a15092559099674567f86d28db838
nikita1803 <rainikita595@gmail.com>
2021-04-21 10:12:02 +0530

    Added Daily Employee Wages

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,5 +1,5 @@
 echo "welcome"
-
+#UC-1
 isPresent=1;
 randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ]
@@ -7,4 +7,14 @@ then
 	echo "Employee is present" ;
 else
 	echo "Employee is absent" ;
+fi
+#UC-2
+if [ $isPresent -eq $randomCheck ]
+then
+     empRatePerday=20;
+     empHrs=8;
+     salary=$(($empRatePerday*$empHrs))
+     echo "Daily employee wage is".$salary
+else
+     salary=0;
 fi
